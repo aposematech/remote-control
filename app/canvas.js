6d8c38fa5e1ce78ad7e64dedfe47d9f01eaed0ca
Jon Faviell <jon.faviell@gmail.com>
2022-08-29 20:40:42 -0500

    add max variable

--- app/canvas.js
+++ app/canvas.js
@@ -8,21 +8,22 @@ var canvas,
 	angle = 0;
 
 function draw() {
+	var max = 90
 	var radius = ((width > height) ? width : height);
 	context.clearRect(0, 0, width, height);
 	context.save();
 	context.translate(width / 2, height / 2);
 	context.rotate(angle / Math.PI);
-	for (var i = 1; i < 40; i++) {
-		x = Math.sin(i / 2) * (radius / 20);
+	for (var i = 1; i < max; i++) {
+		x = Math.sin(i / 2) * (radius / (max / 2));
 		context.beginPath();
 		if (i % 2 !== 0) {
 		context.fillStyle = "#000";
-		context.arc(x, y, radius - i * (radius / 40), 0, 2 * Math.PI, false);
+		context.arc(x, y, radius - i * (radius / max), 0, 2 * Math.PI, false);
 		context.fill();
 		} else {
 		context.fillStyle = "#fff";
-		context.arc(x, y, radius - i * (radius / 40), 0, 2 * Math.PI, false);
+		context.arc(x, y, radius - i * (radius / max), 0, 2 * Math.PI, false);
 		context.fill();
 		}
 	}
