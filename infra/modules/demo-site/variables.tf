416fa61f026990642469d804c5a81d72d59deeeb
Jon Faviell <jon.faviell@gmail.com>
2022-09-01 16:31:14 -0500

    move to demo site module

---
+++ infra/modules/demo-site/variables.tf
@@ -0,0 +1,17 @@
+variable "region" {
+  description = "AWS region"
+  type        = string
+  default     = ""
+}
+
+variable "registered_domain_name" {
+  description = "Route 53 registered domain name"
+  type        = string
+  default     = ""
+}
+
+variable "default_page" {
+  description = "Static website default page"
+  type        = string
+  default     = ""
+}
