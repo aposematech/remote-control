77b28855348c171222ea0dede34736c5d961d245
Jon Faviell <jon.faviell@gmail.com>
2022-09-04 20:02:15 -0500

    fix browser check script

--- infra/modules/site-monitors/main.tf
+++ infra/modules/site-monitors/main.tf
@@ -124,18 +124,22 @@ resource "checkly_check" "browser_check" {
   runtime_id = "2022.02"
 
   script = <<EOT
-const browser = await chromium.launch()
-const page = await browser.newPage()
+const { chromium } = require('playwright')
+async function run () {
+  const browser = await chromium.launch()
+  const page = await browser.newPage()
 
-const response = await page.goto('https://${var.registered_domain_name}')
+  const response = await page.goto('https://${var.registered_domain_name}')
 
-if (response.status() > 399) {
-  throw new Error(`Failed with response code $${response.status()}`)
+  if (response.status() > 399) {
+    throw new Error(`Failed with response code $${response.status()}`)
+  }
+
+  await page.screenshot({ path: 'screenshot.jpg' })
+
+  await page.close()
+  await browser.close()
 }
-
-await page.screenshot({ path: 'screenshot.jpg' })
-
-await page.close()
-await browser.close()
+run()
 EOT
 }
