26ad68e2b50b93712af18b2897638f905b1d052f
Jon Faviell <jon.faviell@gmail.com>
2022-11-20 00:25:50 -0600

    remove output

--- infra/modules/ops/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "betteruptime_status_page_id" {
-  value       = betteruptime_status_page.status_page.id
-  description = "Better Uptime Status Page ID"
-}
--- infra/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "betteruptime_status_page_id" {
-  value       = module.ops.betteruptime_status_page_id
-  description = "Better Uptime Status Page ID"
-}
