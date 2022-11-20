171357f4820276280070f939e3ca406975682c65
Jon Faviell <jon.faviell@gmail.com>
2022-11-20 00:18:29 -0600

    add output

--- infra/modules/ops/outputs.tf
+++ infra/modules/ops/outputs.tf
@@ -1,4 +1,4 @@
 output "betteruptime_status_page_id" {
-  value = betteruptime_status_page.status_page.id
+  value       = betteruptime_status_page.status_page.id
   description = "Better Uptime Status Page ID"
 }
---
+++ infra/outputs.tf
@@ -0,0 +1,4 @@
+output "betteruptime_status_page_id" {
+  value       = module.ops.betteruptime_status_page_id
+  description = "Better Uptime Status Page ID"
+}
