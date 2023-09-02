a61aadbc2f501c5e74ee557c918d04dea076d62f
Jon Faviell <jon.faviell@gmail.com>
2023-09-02 14:52:43 -0500

    edit qr code script

--- scripts/qrcodegen.py
+++ scripts/qrcodegen.py
@@ -2,14 +2,19 @@
 
 # https://pypi.org/project/qrcode/
 import qrcode
+from PIL import Image, ImageOps
 
 qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
-    box_size=20,
-    border=4,
+    box_size=50,
+    border=5,
 )
 qr.add_data("https://remotecontrol.solutions")
 qr.make(fit=True)
 img = qr.make_image(fill_color="black", back_color="white")
 img.save("remotecontrol.png")
+ImageOps.expand(Image.open('remotecontrol.png'),border=1,fill='black').save('remotecontrol_border.png')
+img = Image.open('remotecontrol_border.png')
+img = img.resize((1500, 1500))
+img.save('remotecontrol_resize.png')
