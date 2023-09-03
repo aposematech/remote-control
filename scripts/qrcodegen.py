273344f8e924dd7fc36edf43d2719708011899ba
Jon Faviell <jon.faviell@gmail.com>
2023-09-02 22:54:17 -0500

    add comments

--- scripts/qrcodegen.py
+++ scripts/qrcodegen.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3.9
 
-# https://pypi.org/project/qrcode/
-import qrcode
-from PIL import Image, ImageOps
+import qrcode                   # https://pypi.org/project/qrcode/
+from PIL import Image, ImageOps # https://pypi.org/project/Pillow/
 
+# generate qrcode
 qr = qrcode.QRCode(
     version=1,
     error_correction=qrcode.constants.ERROR_CORRECT_L,
@@ -14,7 +14,9 @@ qr.add_data("https://remotecontrol.solutions")
 qr.make(fit=True)
 img = qr.make_image(fill_color="black", back_color="white")
 img.save("remotecontrol.png")
+# add border
 ImageOps.expand(Image.open('remotecontrol.png'),border=1,fill='black').save('remotecontrol_border.png')
+# resize
 img = Image.open('remotecontrol_border.png')
 img = img.resize((1500, 1500))
 img.save('remotecontrol_resize.png')
