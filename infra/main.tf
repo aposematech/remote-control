e2ed2d9761f619cc1c84913139e07d2dd207ace4
Jon Faviell <jon.faviell@gmail.com>
2023-09-19 18:46:51 -0500

    remove import

--- infra/main.tf
+++ infra/main.tf
@@ -1,8 +1,3 @@
-import {
-  to = module.web.aws_s3_bucket.website_bucket
-  id = "djfav.ninja"
-}
-
 module "git" {
   source                  = "./modules/git"
   git_repo_name           = terraform.workspace
